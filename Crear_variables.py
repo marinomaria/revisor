306b2b38d238b5802a46158e01f6a97a664a76f4
marinomaria <44098774+marinomaria@users.noreply.github.com>
2019-01-31 16:54:28 -0300

    Add files via upload

--- Crear_variables.py
+++ Crear_variables.py
@@ -43,14 +43,24 @@ del df_N['comm']
 del df_N['omit']
 del df_N['tester']
 del df_N['id']
+
+dicc2 = np.array(['id', 'completo', 'orientacion_politica', 'polarizacion', 'zero'])
+df_users = pd.DataFrame(data = Us, columns = dicc2)
+df_users.set_index('id', inplace = True)
+df_users.index = df_users.index.astype(int, copy = False)
+del df_users['completo']
+del df_users['zero']
 #%%
 repreguntas = np.array(np.where(df_N['es_repr'] == 1))[0] #Array de repreguntas
 
 statements = np.array([])
-varC = np.array([])
-varA = np.array([])
+variacion_confianza = np.array([])
+variacion_agreement = np.array([])
 treatment = np.array([])
-
+distancia = np.array([])
+deteccion = np.array([])
+#timer = np.array([])
+#%%
 def buscar_statement(repregunta):
     return np.where((df_N['qst_id'] == df_N.loc[repregunta]['qst_id']) & (df_N['user_id'] == df_N.loc[repregunta]['user_id']) & (df_N['es_repr'] == 0))
 
@@ -65,13 +75,41 @@ def crear_variable_treatment(index_repregunta):
     if df_N.loc[index_repregunta]['answer'] != df_N.loc[index_repregunta]['valor_presentado']:
         return True
     else: return False
- #%%   
+    
+def crear_distancia(repregunta, statement):
+    return repregunta - statement
+
+def crear_deteccion(index_repregunta):
+    if df_N.loc[index_repregunta]['answer_repr'] != -1:
+        return True
+    else: return False
+    
+def crear_genero():
+    return np.array(df_N.loc[np.where(df_N['qst_id'] == 29)[0]]['answer'])
+
+def crear_edad():
+    return np.array(df_N.loc[np.where(df_N['qst_id'] == 30)[0]]['answer'])
+
+#def crear_timer(index_repregunta):
+#    if df_N.loc[index_repregunta]['timer'] != 0:
+#        return df_N.loc[index_repregunta]['timer']
+#    else: return np.nan   
+#%%   
 for repregunta in repreguntas:
     statements = np.append(statements, buscar_statement(repregunta))
-    varA = np.append(varA, crear_variacion_de_agreement(repregunta))
+    variacion_agreement = np.append(variacion_agreement, crear_variacion_de_agreement(repregunta))
     treatment = np.append(treatment, crear_variable_treatment(repregunta))
+    deteccion = np.append(deteccion, crear_deteccion(repregunta))
     
 for repregunta, statement in zip(repreguntas, statements):
-    varC = np.append(varC, crear_variacion_de_confianza(repregunta, statement))
+    variacion_confianza = np.append(variacion_confianza, crear_variacion_de_confianza(repregunta, statement))
+    distancia = np.append(distancia, crear_distancia(repregunta, statement))
     
-        
+df_users['genero'] = crear_genero()
+df_users['genero'] = df_users['genero'].map({1: 'fem', 2: 'masc'})
+genero = df_users['genero']
+
+edad = crear_edad()
+df_users['edad'] = edad
+
+polarizacion = df_users['polarizacion']
