55de962661ed4a98b263817f5970fd407c942041
marinomaria <44098774+marinomaria@users.noreply.github.com>
2019-01-31 19:43:35 -0300

    Update
    Script SIN FUNCIONES

--- Variables.py
+++ Variables.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import pandas as pd
 import statsmodels.api as sm
-
+import variables as v
 #%%
 #Levanto los datos y los mergeo
 Datos = np.load('MatrizLimpiaConCerosmx.npz')
@@ -50,6 +50,10 @@ df_users.set_index('id', inplace = True)
 df_users.index = df_users.index.astype(int, copy = False)
 del df_users['completo']
 del df_users['zero']
+df_users['edad'] = v.crear_edad()
+df_users['genero'] = v.crear_genero()
+df_users['genero'] = df_users['genero'].map({1: 'fem', 2: 'masc'})
+
 #%%
 repreguntas = np.array(np.where(df_N['es_repr'] == 1))[0] #Array de repreguntas
 
@@ -60,56 +64,18 @@ treatment = np.array([])
 distancia = np.array([])
 deteccion = np.array([])
 #timer = np.array([])
-#%%
-def buscar_statement(repregunta):
-    return np.where((df_N['qst_id'] == df_N.loc[repregunta]['qst_id']) & (df_N['user_id'] == df_N.loc[repregunta]['user_id']) & (df_N['es_repr'] == 0))
-
-def crear_variacion_de_confianza(index_repregunta, index_statement):
-    return abs(df_N.loc[index_repregunta]['confianza'] - df_N.loc[index_statement]['confianza'])
-    
-def crear_variacion_de_agreement(index_repregunta):
-    if df_N.loc[index_repregunta]['answer_repr'] != -1:
-        return abs(df_N.loc[index_repregunta]['answer'] - df_N.loc[index_repregunta]['answer_repr'])
-    
-def crear_variable_treatment(index_repregunta):
-    if df_N.loc[index_repregunta]['answer'] != df_N.loc[index_repregunta]['valor_presentado']:
-        return True
-    else: return False
-    
-def crear_distancia(repregunta, statement):
-    return repregunta - statement
-
-def crear_deteccion(index_repregunta):
-    if df_N.loc[index_repregunta]['answer_repr'] != -1:
-        return True
-    else: return False
-    
-def crear_genero():
-    return np.array(df_N.loc[np.where(df_N['qst_id'] == 29)[0]]['answer'])
-
-def crear_edad():
-    return np.array(df_N.loc[np.where(df_N['qst_id'] == 30)[0]]['answer'])
-
-#def crear_timer(index_repregunta):
-#    if df_N.loc[index_repregunta]['timer'] != 0:
-#        return df_N.loc[index_repregunta]['timer']
-#    else: return np.nan   
 #%%   
 for repregunta in repreguntas:
-    statements = np.append(statements, buscar_statement(repregunta))
-    variacion_agreement = np.append(variacion_agreement, crear_variacion_de_agreement(repregunta))
-    treatment = np.append(treatment, crear_variable_treatment(repregunta))
-    deteccion = np.append(deteccion, crear_deteccion(repregunta))
+    statements = np.append(statements, v.buscar_statement(repregunta))
+    variacion_agreement = np.append(variacion_agreement, v.crear_variacion_de_agreement(repregunta))
+    treatment = np.append(treatment, v.crear_variable_treatment(repregunta))
+    deteccion = np.append(deteccion, v.crear_deteccion(repregunta))
     
 for repregunta, statement in zip(repreguntas, statements):
-    variacion_confianza = np.append(variacion_confianza, crear_variacion_de_confianza(repregunta, statement))
-    distancia = np.append(distancia, crear_distancia(repregunta, statement))
+    variacion_confianza = np.append(variacion_confianza, v.crear_variacion_de_confianza(repregunta, statement))
+    distancia = np.append(distancia, v.crear_distancia(repregunta, statement))
     
-df_users['genero'] = crear_genero()
-df_users['genero'] = df_users['genero'].map({1: 'fem', 2: 'masc'})
+
 genero = df_users['genero']
-
-edad = crear_edad()
-df_users['edad'] = edad
-
+edad = df_users['edad']
 polarizacion = df_users['polarizacion']
