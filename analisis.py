3de7cd9da76fc06d0ae1495ff7a3c9a1e406f7c6
juanwisz <juanwisznia@gmail.com>
2019-04-30 17:49:49 -0300

    script de ploteo, soy juan_capo96

---
+++ analisis.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 20:15:06 2019
+
+@author: Casa
+"""
+#%%
+import numpy as np
+import pandas as pd
+import funciones as v
+#%%
+df_repr = pd.read_pickle('Repreguntas')
+#%%
+
+
