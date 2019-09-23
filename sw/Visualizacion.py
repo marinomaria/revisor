3c67f24995f106f6a0b123210e362d31a1b1eca8
RoccoDiTella <roccoditella1@gmail.com>
2019-09-23 14:26:37 -0300

    Pearson y falta columna deteccion

--- sw/Visualizacion.py
+++ sw/Visualizacion.py
@@ -1,16 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 14 14:39:41 2019
-
-@author: Milt
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.stats as sc
 
+sw = pd.read_csv('SW.csv', index_col = 0)
+#%%
+
+def manifun(user_id, df):
+    if df.
+sw['bla'] = [manifun(x) for x in sw.index]
 #%% Manipulados y no manipulados fork 1 y 3
-sw = pd.read_csv('SWperez.csv', index_col = 0)
+
 plt.figure(1)
 plt.title('Manipulados')
 plt.plot(np.mean([sw.A1[(sw.fork == 1) | (sw.fork == 3)],sw.A2[(sw.fork == 1) | (sw.fork == 3)]],axis = 0), np.mean([sw.A3[(sw.fork == 1) | (sw.fork == 3)],sw.A4[(sw.fork == 1) | (sw.fork == 3)]],axis = 0),'o')
@@ -47,3 +48,17 @@ plt.title('All topics')
 plt.scatter(['0-10','11-20','21-30','31-40','41-50','51-60','61-70','71-80','81-90','91-100'],hist, label = 'Non manipulated')
 plt.scatter(['0-10','11-20','21-30','31-40','41-50','51-60','61-70','71-80','81-90','91-100'],histNM, label = 'Manipulated')
 plt.legend()
+
+#%%
+plt.figure()
+rFork1=sc.pearsonr(sw.A1[(sw.fork == 1)],sw.A4[(sw.fork == 1)])
+plt.plot(sw.A1[(sw.fork == 1)], sw.A4[(sw.fork == 1)],'o',alpha=0.2, label='r = %.3f'%rFork1[0])
+plt.title('manipulados A1 vs A4')
+plt.legend(loc='upper left')
+plt.figure()
+rFork2=sc.pearsonr(sw.A1[(sw.fork == 2)],sw.A4[(sw.fork == 2)])
+plt.plot(sw.A1[(sw.fork == 2)], sw.A4[(sw.fork == 2)],'o',alpha=0.2, label='r = %.3f'%rFork2[0])
+plt.title('no manipulados A1 vs A4')
+plt.legend(loc='upper left')
+#%%
+plt.plot(sw.A3[(sw.fork == 2) | (sw.fork == 4)], sw.A4[(sw.fork == 2) | (sw.fork == 4)],'o',alpha=0.2)
